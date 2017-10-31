4757cc727a548a1364edd409802c65ab4b038b03
chenm <chenm@axon.com.cn>
2017-10-31 15:41:23 +0800

    commit

---
+++ src/pages/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
